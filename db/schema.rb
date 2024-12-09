55a784b88163c931036dcc3f87a571c203d075c7
ThomasDebek <tom.debek@gmail.com>
2024-12-09 22:09:51 +0100

    model: Line - rails generate model Line name:string type_of_vehicle:string low_floor:boolean

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_12_09_210715) do
+ActiveRecord::Schema[8.0].define(version: 2024_12_09_210933) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "lines", force: :cascade do |t|
+    t.string "name"
+    t.string "type_of_vehicle"
+    t.boolean "low_floor"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "stops", force: :cascade do |t|
     t.string "name"
