3a61644fda856464bbfcfce0624df536c5f75f27
ThomasDebek <tom.debek@gmail.com>
2024-12-11 19:49:31 +0100

    routes: new path

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,20 @@
 Rails.application.routes.draw do
-  get "lines/index"
-  get "lines/show"
-  get "stops/index"
-  get "stops/show"
-  resources :stops, only: [:index, :show]
-  resources :lines, only: [:index, :show]
+  root "stops#index" # Strona główna aplikacji
+
+  resources :stops, only: [:index, :show] do
+    member do
+      get :lines # Wyświetla linie obsługujące dany przystanek
+    end
+  end
+
+  resources :lines, only: [:index, :show] do
+    member do
+      get :stops # Wyświetla przystanki na trasie danej linii
+    end
+  end
+
+  resources :schedules, only: [:index] # Wyświetla wszystkie rozkłady jazdy (opcjonalne)
+
+  # Obsługa błędnych tras
+  get "*path", to: "stops#index", constraints: ->(req) { req.path.exclude? "rails/active_storage" }
 end
