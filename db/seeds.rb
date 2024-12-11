4ab618ac2c8dbfa0bee96fe3aed342cde9e615d1
ThomasDebek <tom.debek@gmail.com>
2024-12-11 19:46:26 +0100

    seed: create some data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,17 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+p "Create new Transporstion and before clear old DB"
+Schedule.destroy_all
+LineStop.destroy_all
+Line.destroy_all
+Stop.destroy_all
+
+
+stop1 = Stop.create(name: "Stop 1", latitude: 40.0, longitude: 45.0, on_demand: false)
+stop2 = Stop.create(name: "Stop 2", latitude: 51.0, longitude: 21.0, on_demand: true)
+
+line1 = Line.create(name: "Line 1", type_of_vehicle: "Bus", low_floor: true)
+
+LineStop.create(line: line1, stop: stop1, sequence: 1)
+LineStop.create(line: line1, stop: stop2, sequence: 2)
+
+Schedule.create(line: line1, stop: stop1, direction: "to City", time: "08:00")
+Schedule.create(line: line1, stop: stop2, direction: "to City", time: "08:10")
